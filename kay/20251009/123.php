33863a292ed88a64e845246a980aedd99979d6e9
raby <aa60772413@gmail.com>
2025-11-07 15:34:58 +0800

    KAY

---
+++ kay/20251009/123.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+echo "Hello, World!";
+print "你好，世界！";
+$name = "小明";
+$age = 18;
+$isStudent = true;
+
+echo $name . "的年齡是" . $age;
+$score =85
+if ($score > 90){
+    echo "甲上";
+    {elseif ($score <60);}
+    echo "乙";
+    else "丙";
+    
+
+}
+    ?>
+</body>
+
+</html>
